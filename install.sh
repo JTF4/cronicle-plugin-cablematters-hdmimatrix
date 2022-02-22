f3039542fad64e30f3aadcbe55e1a84711741969
David Stevens <teamjtf4@gmail.com>
2022-02-22 18:10:54 -0500

    Installer Setup

--- install.sh
+++ install.sh
@@ -2,6 +2,10 @@
 #!/bin/sh
 echo "Installing Plugin"
 
+cd ..
+
+rm -rf cronicle-plugin-snapav-wattbox
+
 npm install cronicle-plugin-snapav-wattbox
 
 mv ./node_modules/cronicle-plugin-snapav-wattbox ./cronicle-plugin-snapav-wattbox
