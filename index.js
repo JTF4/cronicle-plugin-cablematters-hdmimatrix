ed281016a631540679d6c32d841c7270e4c10e49
David Stevens <teamjtf4@gmail.com>
2022-03-28 14:41:20 -0400

    Custom reset command

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ process.stdin.on('data', (res) => {
 		let ip = data['params']['ip'];
 		let outlet = data['params']['outlet'];
 		let command = data['params']['command'];
+		let extraData = data['params']['extraData'];
 		let username = data['params']['username'];
 		let password = data['params']['password'];
 
@@ -42,53 +43,121 @@ process.stdin.on('data', (res) => {
 		switch (command) {
 			case 'Power Off':
 				commandUrl = `/control.cgi?outlet=${outlet}&command=0`;
+				executeCommand(commandUrl);
 				break;
 			case 'Power On':
 				commandUrl = `/control.cgi?outlet=${outlet}&command=1`;
+				executeCommand(commandUrl);
+				break;
+			case 'Custom Power Reset':
+				commandUrl = `/control.cgi?outlet=${outlet}&command=3`;
+				if (!extraData) {
+					extraData = '5000';
+				}
+				// Turn the switch off
+				commandUrl = `/control.cgi?outlet=${outlet}&command=0`;
+				executeCommandNoExit(commandUrl);
+
+				// Turn the switch on
+				setTimeout(() => {
+					commandUrl = `/control.cgi?outlet=${outlet}&command=1`;
+					executeCommand(commandUrl);
+				}, extraData);
 				break;
 			case 'Power Reset':
 				commandUrl = `/control.cgi?outlet=${outlet}&command=3`;
+				executeCommand(commandUrl);
 				break;
 			case 'Auto Reboot On':
 				commandUrl = `/control.cgi?outlet=0&command=4`;
+				executeCommand(commandUrl);
 				break;
 			case 'Auto Reboot Off':
 				commandUrl = `/control.cgi?outlet=0&command=5`;
+				executeCommand(commandUrl);
 				break;
 		}
-
-		let client = new Client();
-
-		let url = 'http://' + ip + commandUrl;
-
-		console.log('making request:', url);
-
-		let args = {
-			headers: {
-				Host: self.config.ip,
-				'Keep-Alive': '300',
-				Connection: 'keep-alive',
-				'User-Agent': 'APP',
-				Authorization: `Basic ${authKey}`,
-			},
-		};
-
-		client
-			.get(url, args, function (data, response) {
-				debug({ data: data, response: response });
-			})
-			.on('error', function (error) {
-				console.log('error response:', error);
-				console.log({ error: error });
-				console.log(`{ "complete": 1, "code": 999, "description": "Failed to execute: ${error}" }`);
-			});
-
-		console.log('{ "complete": 1 }');
 	} catch (err) {
 		console.log(err);
-		console.log('{ "complete": 1, "code": 999, "description": "Failed to execute." }');
+		console.log({ error: error });
+		console.log(`{ "complete": 1, "code": 999, "description": "Failed to execute: ${err}" }`);
 	}
 });
+
+/**
+ * It sends a GET request to the server with the command URL.
+ * @param commandUrl - The URL to the command.
+ */
+
+function executeCommand(commandUrl) {
+	let url = 'http://' + ip + commandUrl;
+	rest_get(url).then((res) => {
+		console.log(res);
+	}).catch((err) => {
+		console.log('error response:', error);
+		console.log({ error: error });
+		console.log(`{ "complete": 1, "code": 999, "description": "Failed to execute: ${error}" }`);
+	}).finally (() => {
+		console.log('Command Complete');
+		console.log('{ "complete": 1 }');
+	});
+}
+
+/**
+ * It executes a command on the server and returns the result.
+ * @param commandUrl - the URL to the command.
+ */
+
+function executeCommandNoExit(commandUrl) {
+	let url = 'http://' + ip + commandUrl;
+	rest_get(url).then((res) => {
+		console.log(res);
+	}).catch((err) => {
+		console.log('error response:', error);
+		console.log({ error: error });
+		console.log(`{ "complete": 1, "code": 999, "description": "Failed to execute: ${error}" }`);
+	})
+}
+
+/**
+ * It sends a GET request to the server.
+ * @param url - The URL to send the request to.
+ * @returns The response from the server.
+ */
+
+function rest_get(url) {
+
+	console.log('sending request to: ' + url);
+
+	let client = new Client();
+
+	let args = {
+		headers: {
+			Host: ip,
+			'Keep-Alive': '300',
+			Connection: 'keep-alive',
+			'User-Agent': 'APP',
+			Authorization: `Basic ${authKey}`,
+		},
+	};
+
+	return new Promise((resolve, reject) => {
+		client
+			.get(url, args, function (data, response) {
+				resolve(data);
+			})
+			.on('error', function (error) {
+				reject(error);
+			});
+	});
+};
+
+/**
+ * Given a username and password, return a base64 encoded string of the username and password
+ * @param username - The username of the user you want to authenticate.
+ * @param password - The password for the user.
+ * @returns The authKey is being returned.
+ */
 
 function getAuthKey(username, password) {
 	let authString = username + ':' + password;
